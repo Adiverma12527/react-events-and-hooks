2042ecee699813fa7362de96f7e1d763a083199e
aditya verma <adityavermasuper.12527@gmail.com>
2023-04-26 21:15:33 -0700

    onclick function on usestate

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
 import './App.css';
-import { Button } from './components/button/button';
-import Card from './components/card/card';
-
+import { useState} from 'react'
 function App() {
-
-  //  components -/> small isolated piece of code of you complete ui .
-  // components  -/> functions (react function)
-  //  js function -/> takes datatype as input and return data types
-  // react function -/> takes props as input and return jsx / html likw components .
-  //  react functions startWith Capital letter only . 
-
-
-//   Jsx -/> javascript extension 
-//   it provides the power to write the html in js file 
-
-//  ui 
-//  functionality 
-//  data management 
-//  folder structure (Atomic folder structure)
-
-
-//  css  -> style to style the component
-//  js -/> code to ceate the component 
-
-
-
-  return (
-    <div className="App">
-      <h1>Welcome to react js  ....</h1>
-      <Button/>
-      <Card/>
-    </div>
-  );
+  const [count,setcount]= useState(0);
+  function handleclick(){
+   const newcount=count+1;
+   setcount(newcount)
+  }
+  function handleclicks(){
+    if(count>0){
+    const newcount=count-1;
+    setcount(newcount)}
+  }
+return(<div className='App'>
+  <h1> increase value{count}</h1>
+  <button onClick={handleclick}> increase the value of count</button>
+  <button onClick={handleclicks}> decrease the value of count </button></div>
+)
 }
 
 export default App;
